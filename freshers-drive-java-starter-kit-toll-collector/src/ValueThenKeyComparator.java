f51d090b28b0672709a758f3bb37fd4659a84c6b
sudip40 <karmakar.sudip40@gmail.com>
2023-01-15 22:52:31 +0530

    Toll Calculator App

---
+++ freshers-drive-java-starter-kit-toll-collector/src/ValueThenKeyComparator.java
@@ -0,0 +1,17 @@
+import java.util.Comparator;
+import java.util.Map;
+
+public class ValueThenKeyComparator<K extends Comparable<? super K>,
+                                    V extends Comparable<? super V>>
+    implements Comparator<Map.Entry<K, V>> {
+
+    public int compare(Map.Entry<K, V> a, Map.Entry<K, V> b) {
+        int cmp1 = b.getValue().compareTo(a.getValue());
+        if (cmp1 != 0) {
+            return cmp1;
+        } else {
+            return a.getKey().compareTo(b.getKey());
+        }
+    }
+
+}
